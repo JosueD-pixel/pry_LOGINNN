9745f06df7c9a134f90b07d80ec26e61a989257a
luis angel mercado hernandez <mercadohernandezluisangel66@gmail.com>
2026-06-10 18:14:35 -0600

    luis angel_BOTON

--- pry_LOGINNN/frmLogin.cs
+++ pry_LOGINNN/frmLogin.cs
@@ -11,5 +11,26 @@ namespace pry_LOGINNN
         {
             Application.Exit();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clsLogin login = new clsLogin();
+                login.Usuario = txtUsuario.Text;
+                login.Password = txtPassword.Text;
+
+                bool resp = login.ValidarAcceso();
+                if (resp == true)
+                {
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();//Cierra el login y abre el formulario principal
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error de autenticacion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
