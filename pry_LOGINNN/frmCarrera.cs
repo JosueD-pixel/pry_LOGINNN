e8e2798ea87f7d15a78091b1715c85c947efab6e
ChristianK <20251047@uthh.edu.mx>
2026-06-17 22:39:54 -0600

    Ventana de carreras listo

---
+++ pry_LOGINNN/frmCarrera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pry_LOGINNN
+{
+    public partial class frmCarrera : Form
+    {
+        public frmCarrera()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCarrera_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
