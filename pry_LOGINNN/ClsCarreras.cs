e7de82713185dcaf31953db691f12d2a3a06df35
luis angel mercado hernandez <mercadohernandezluisangel66@gmail.com>
2026-06-19 11:51:38 -0600

    cambios de git_luis angel

--- pry_LOGINNN/ClsCarreras.cs
+++ pry_LOGINNN/ClsCarreras.cs
@@ -9,23 +9,38 @@ namespace pry_LOGINNN
 {
     internal class ClsCarreras
     {
+        private string nombreCarrera;
         //Usamos un adaptador 
         private MySqlDataAdapter consulta;
         //usamos una tabla temporal
         private DataTable tabla;
 
+        //Propiedad para el atributo nombreCarrera
+        public string NombreCarrera { get => nombreCarrera; set => nombreCarrera = value; }
+
         //Metodo para cargar datos en el DataGrid
         public DataTable CargarDataGrid()
         {
             tabla = new DataTable();
-
-            cls_conexion conexionBD = new cls_conexion();
-            var conexion = conexionBD.AbrirConexion();
-            string sql = "select idCarrera AS Clave, nombreCarrera AS Carrera, descripcion AS Descripcion  from tblCarreras;";
-            consulta = new MySqlDataAdapter(sql, conexion);
-            consulta.Fill(tabla);
+            try
+            {
+                cls_conexion conexionBD = new cls_conexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "select idCarrera AS Clave, nombreCarrera AS Carrera, descripcion AS Descripcion  from tblCarreras;";
+                    using (consulta = new MySqlDataAdapter(sql, conexion))
+                    {
+                        consulta.Fill(tabla);
+                    }//Liberar la consulta
+                }//Liberarla conexion
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en la conexion " + ex.Message);
+            }
             return tabla;
-        }
 
+
+        }
     }
 }
