c7af5cc4930f153bdb11f05f0dc7bde085c2d360
luis angel mercado hernandez <mercadohernandezluisangel66@gmail.com>
2026-06-24 10:57:54 -0600

    autor_Luis angel

--- pry_LOGINNN/clsTutores.cs
+++ pry_LOGINNN/clsTutores.cs
@@ -52,6 +52,126 @@ namespace pry_LOGINNN
             }
             return tabla;
         }
+        public DataTable Consultar()
+        {
+            tabla = new DataTable();
+            try
+            {
+                cls_conexion conexionBD = new cls_conexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT idTutor AS Clave, nombreTutor AS Tutor, parentesco AS Parentesco, direccion AS Direccion, telefono AS Telefono, correo AS Correo FROM tbltutores WHERE nombreTutor LIKE @tutor;";
+                    using (var consultar = new MySqlCommand(sql, conexion))
+                    {
+                        consultar.Parameters.AddWithValue("@tutor", "%" + nombreTutor + "%");
+                        using (consulta = new MySqlDataAdapter(consultar))
+                        {
+                            consulta.Fill(tabla);
+                        }//Liberar el adaptador
+                    }//Liberar la consulta
+                }//Liberar la conexion
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en la conexion de la base de datos " + ex.Message);
+            }
+            return tabla;
+        }
+        public string Eliminar()
+        {
+            string msg = "";
+            try
+            {
+                cls_conexion conexionBD = new cls_conexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "DELETE FROM tbltutores C WHERE C.idTutor = @idTutor;";
+                    using (comando = new MySqlCommand(sql, conexion))
+                    {
+                        comando.Parameters.AddWithValue("@idTutor", idTutor);
+                        int filasAfectadas = comando.ExecuteNonQuery();
+                        if (filasAfectadas > 0)
+                        {
+                            msg = "Datos eliminados correctamente";
+                        }
+                        else
+                        {
+                            msg = "Los datos no se pudieron eliminar";
+                        }
+                    }//liberar las conexiones
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error" + ex.Message);
+            }
+            return msg;
+        }
+        public string GuardarActualizar(int TipoOperacion)
+        {
+
+            string msg = "";
+            try
+            {
+                cls_conexion conexionBD = new cls_conexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    switch (TipoOperacion)
+                    {
+                        case 0://insertarNEW
+                            string sqlN = "INSERT INTO tbltutores(nombreTutor, parentesco, direccion, telefono, correo) VALUES (@nombreTutor, @parentesco, @direccion, @telefono, @correo);";
+                            using (comando = new MySqlCommand(sqlN, conexion))
+                            {
+                                comando.Parameters.AddWithValue("nombreTutor", nombreTutor);
+                                comando.Parameters.AddWithValue("parentesco", parentesco);
+                                comando.Parameters.AddWithValue("direccion", direccion);
+                                comando.Parameters.AddWithValue("telefono", telefono);
+                                comando.Parameters.AddWithValue("correo", correo);
+
+                                int filasAfectadas = comando.ExecuteNonQuery();
+                                if (filasAfectadas > 0)
+                                {
+                                    msg = "El registro se guardo correctamente";
+                                }
+                                else
+                                {
+                                    msg = "Error, No se guardaron los datos";
+                                }
+                            }//Liberar la operacion de insercion
+                            break;
+                        case 1://ActualizarOLD
+                            string sqlA = "UPDATE tbltutores C SET C.nombreTutor = @nombreTutor, C.parentesco = @parentesco, C.direccion = @direccion, C.telefono = @telefono, C.correo = @correo WHERE C.idTutor = @idTutor;";
+                            using (comando = new MySqlCommand(sqlA, conexion))
+                            {
+                                comando.Parameters.AddWithValue("idTutor", idTutor);
+                                comando.Parameters.AddWithValue("nombreTutor", nombreTutor);
+                                comando.Parameters.AddWithValue("parentesco", parentesco);
+                                comando.Parameters.AddWithValue("direccion", direccion);
+                                comando.Parameters.AddWithValue("telefono", telefono);
+                                comando.Parameters.AddWithValue("correo", correo);
+                                int filasAfectadas = comando.ExecuteNonQuery();
+                                if (filasAfectadas > 0)
+                                {
+                                    msg = "El registro se actualizo correctamente";
+                                }
+                                else
+                                {
+                                    msg = "Error, No se actualizaron los datos";
+                                }
+                            }//Liberar la operacion de actualizacion
+                            break;
+                    }
+                }//Libera la conexion
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error" + ex.Message);
+            }
+            return msg;
+        }
+
+
+
 
 
     }
