85951358cd26d0b0e6ae9f908628e67706d0a874
Angel Josue Domingo Guillen <angel_domingoguillen07@hotmail.com>
2026-06-23 23:42:29 -0600

    Hecho por angel josue

--- pry_LOGINNN/frmTutores.cs
+++ pry_LOGINNN/frmTutores.cs
@@ -47,5 +47,86 @@ namespace pry_LOGINNN
             }
         }
 
+        private void dgvTutores_SelectionChanged(object sender, EventArgs e)
+        {
+            //campo oculto que me servira de referencia y actualizar 
+            idTutor = int.Parse(dgvTutores.CurrentRow.Cells[0].Value.ToString());
+            //visuales
+            txtNombre.Text = dgvTutores.CurrentRow.Cells[1].Value.ToString();
+            txtParentesco.Text = dgvTutores.CurrentRow.Cells[2].Value.ToString();
+            txtDireccion.Text = dgvTutores.CurrentRow.Cells[3].Value.ToString();
+            txtTelefono.Text = dgvTutores.CurrentRow.Cells[4].Value.ToString();
+            txtCorreo.Text = dgvTutores.CurrentRow.Cells[5].Value.ToString();
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            idTutor = 0;
+            txtNombre.Clear();
+            txtParentesco.Clear();
+            txtDireccion.Clear();
+            txtTelefono.Clear();
+            txtCorreo.Clear();
+            txtNombre.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int tipoOperacion = idTutor == 0 ? 0 : 1;
+
+                tutores.IdTutor = idTutor;
+                tutores.NombreTutor = txtNombre.Text;
+                tutores.Parentesco = txtParentesco.Text;
+                tutores.Direccion = txtDireccion.Text;
+                tutores.Telefono = txtTelefono.Text;
+                tutores.Correo = txtCorreo.Text;
+
+                string msg = "";
+                if (tipoOperacion != 0)
+                {
+                    var resp = MessageBox.Show("Confirmar que desea guardar el dato seleccionado", "ALERTA", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (resp == DialogResult.Yes)
+                    {
+                        msg = tutores.GuardarActualizar(tipoOperacion);
+                        MessageBox.Show(msg);
+                    }
+                }
+                else
+                {
+                    msg = tutores.GuardarActualizar(tipoOperacion);
+                    MessageBox.Show(msg);
+                }
+                CargarGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tutores.IdTutor = idTutor;
+                var resp = MessageBox.Show("Confirmar que desea eliminar el dato seleccionado", "ALERTA!!", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                if (resp == DialogResult.Yes)
+                {
+                    string msg = tutores.Eliminar();
+                    MessageBox.Show(msg);
+                    CargarGrid();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
     }
 }
