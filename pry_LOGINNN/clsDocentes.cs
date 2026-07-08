b0c59abd73e48224df7715c2b1f2472dd01ee55c
nathaan-hz <20250987@uthh.edu.mx>
2026-07-07 19:34:55 -0600

    autor: nathan metodos consultar y eliminar btn eliminar y evento txtchange

--- pry_LOGINNN/clsDocentes.cs
+++ pry_LOGINNN/clsDocentes.cs
@@ -22,6 +22,8 @@ namespace pry_LOGINNN
         public int IdUsuario { get => idUsuario; set => idUsuario = value; }
 
         private DataTable tabla;
+        private MySqlDataAdapter consulta;
+        private MySqlCommand comando;
 
         public DataTable CargarDataGrid()
         {
@@ -55,6 +57,88 @@ namespace pry_LOGINNN
                 throw new Exception("Error en la conexion al cargar catalogo de docentes " + ex.Message);
             }
             return tabla;
+        }
+        public DataTable Consultar()
+        {
+            tabla = new DataTable();
+            try
+            {
+                cls_conexion conexionBD = new cls_conexion();
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT A.claveDocente AS Clave, " +
+                                 "A.nombreDocente AS Nombre, " +
+                                 "A.puesto AS Puesto, " +
+                                 "U.vchnombreUsuario AS Usuario, " +
+                                 "A.telefono AS Telefono, " +
+                                 "A.correo AS Correo, " +
+                                 "U.vchpassword, " + // <-- Aqui se agrega el password
+                                 "U.vchperfil, " + // <-- Aqui se agrega el perfil
+                                 "A.idUsuario " +
+                                 "FROM tbldocentes A " +
+                                 "INNER JOIN tblusuarios U ON A.idUsuario = U.intidUsuario WHERE claveDocente LIKE @claveDocente;";
+                    using (var consultar = new MySqlCommand(sql, conexion))
+                    {
+                        consultar.Parameters.AddWithValue("@claveDocente", "%" + clave + "%");
+                        using (consulta = new MySqlDataAdapter(consultar))
+                        {
+                            consulta.Fill(tabla);
+                        }//Liberar el adaptador
+                    }//Liberar la consulta
+                }//Liberar la conexion
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en la conexion de la base de datos " + ex.Message);
+            }
+            return tabla;
+        }
+
+        public string Eliminar()
+        {
+            string msg = "";
+            cls_conexion conexionBD = new cls_conexion();
+
+            try
+            {
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    using (var transaccion = conexion.BeginTransaction())
+                    {
+                        try
+                        {
+                            string sqlDelDocente = " DELETE FROM tbldocentes WHERE claveDocente = @claveDocente;";
+                            using (comando = new MySqlCommand(sqlDelDocente, conexion, transaccion))
+                            {
+                                comando.Parameters.AddWithValue("@claveDocente", clave);
+                                comando.ExecuteNonQuery();
+                            }
+
+                            string sqlDelUsuario = " DELETE FROM tblusuarios WHERE intidUsuario = @idUsuario;";
+                            using (comando = new MySqlCommand(sqlDelUsuario, conexion, transaccion))
+                            {
+                                comando.Parameters.AddWithValue("@idUsuario", idUsuario);
+                                comando.ExecuteNonQuery();
+                            }
+
+                            transaccion.Commit();
+                            msg = "El docente y sus credenciales de usuario han sido eliminados del sistema.";
+                        }
+                        catch (Exception ex)
+                        {
+                            //Si algo falla, deshacemos la operacion para no dejar datos huerfanos
+                            transaccion.Rollback();
+                            throw new Exception("No se pudo completar la eliminacion. Cambios revertidos: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error de conexion al eliminar: " + ex.Message);
+            }
+
+            return msg;
         }
 
     }
--- pry_LOGINNN/frmDocentes.cs
+++ pry_LOGINNN/frmDocentes.cs
@@ -11,10 +11,12 @@ namespace pry_LOGINNN
     public partial class frmDocentes : Form
     {
         clsDocentes docentes;
+        int idclave;
+        int idUsuario;
 
         public frmDocentes()
         {
-            clsDocentes docentes;
+            InitializeComponent();
             CargarGrid();
 
         }
@@ -41,5 +43,54 @@ namespace pry_LOGINNN
             }
         }
 
+        private void txtClaveDocente_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtClaveDocente.Text))
+            {
+                CargarGrid();
+                return;
+            }
+            docentes = new clsDocentes();
+            dgvDocentes.DataSource = null;
+            dgvDocentes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            try
+            {
+                docentes.Clave = int.Parse(txtClaveDocente.Text);
+                dgvDocentes.DataSource = docentes.Consultar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            var respuesta = MessageBox.Show($"¿Estas completamente seguro de eliminar permanentemente al docente con Clave: {idclave}?\nEsta accion borrara tambien su cuenta de usuario.",
+                        "¡ADVERTENCIA!", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+
+            if (respuesta == DialogResult.Yes)
+            {
+                try
+                {
+                    docentes = new clsDocentes();
+                    docentes.Clave = idclave;
+                    docentes.IdUsuario = idUsuario;
+
+                    string resultado = docentes.Eliminar();
+
+                    MessageBox.Show(resultado, "Registro Eliminado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    CargarGrid();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrio un error al intentar eliminar el registro: " + ex.Message,
+                                    "Error Operacional", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
     }
 }
